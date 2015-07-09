447fee039c0950e546aacc2e99c27af5f6581e0c
Chris Beck <beck.ct@gmail.com>
2015-07-09 06:47:53 -0400

    update build scripts and gitignore

--- clean_build-clang.sh
+++ clean_build-clang.sh
@@ -5,7 +5,6 @@ mkdir build
 cd build
 cmake -DCMAKE_C_FLAGS="-Qunused-arguments" -DCMAKE_CXX_FLAGS="-std=c++11 -fpermissive -Wno-\#pragma-messages -Qunused-arguments -Wno-unknown-warning-option -Wno-deprecated-register" -DCMAKE_C_COMPILER=/usr/bin/clang-3.6 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-3.6 ..
 make -j3 VERBOSE=1
-cd ..
 export TOOL=gdb #valgrind
-$TOOL ./build/CEGUI_Samples.bin.x86_64
+$TOOL ./CEGUI_Samples.bin.x86_64
 
--- clean_build.sh
+++ clean_build.sh
@@ -6,6 +6,5 @@ cd build
 cmake -DCMAKE_CXX_FLAGS="-std=c++11 -fpermissive -Wno-pragma-messages" -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ ..
 #cmake -DCMAKE_CXX_FLAGS="-std=c++14 -fpermissive" -DCMAKE_C_COMPILER="/usr/bin/clang-3.6" -DCMAKE_CXX_COMPILER="/usr/bin/clang++-3.6" ..
 make -j3 VERBOSE=1
-cd ..
 export TOOL=gdb #valgrind
-$TOOL ./build/CEGUI_Samples.bin.x86_64
+$TOOL ./CEGUI_Samples.bin.x86_64
