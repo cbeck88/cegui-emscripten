a9315756e9acb300d9693adaa5d2fb4f09d9f1fd
Chris Beck <beck.ct@gmail.com>
2015-07-10 16:04:40 -0400

    fixup GLES2_headers

--- lib/cegui/src/RendererModules/OpenGLES2/GLES2_headers.hpp
+++ lib/cegui/src/RendererModules/OpenGLES2/GLES2_headers.hpp
@@ -1,33 +1,6 @@
 #pragma once
-//#include <SDL2/SDL_opengl.h>
 
-//#include "GL/glew.h"
-
-#include <SDL2/SDL.h>
-#define GL_GLEXT_PROTOTYPES
-# include <SDL2/SDL_opengles2.h>
-/*
-#define GL_GLEXT_PROTOTYPES
 #include <GLES2/gl2.h>
+#define GL_GLEXT_PROTOTYPES
 #include <GLES2/gl2ext.h>
 
-#define GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER 0x8CDB
-#define GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER 0x8CDC
-#define GL_FRAMEBUFFER_UNSUPPORTED        0x8CDD
-#define GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER_EXT 0x8CDB
-#define GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER_EXT 0x8CDC
-#define GL_FRAMEBUFFER_UNSUPPORTED_EXT    0x8CDD
-
-#define GLdouble     GLfloat
-*/
-//#include <GL/glew.h>
-/*
-#define GLdouble     GLfloat
-#define GL_CLAMP     GL_CLAMP_TO_EDGE
-#define glClearDepth glClearDepthf
-#define glOrtho      glOrthof
-#define glFrustum    glFrustumf
-
-#define glColor4fv(a) glColor4f(a[0], a[1], a[2], a[3])
-#define glColor3fv(a) glColor4f(a[0], a[1], a[2], 1.0f)
-#define glColor3f(a,b,c) glColor4f(a, b, c, 1.0f)*/
